dc9d83368767e578bd426f2272754f9dbd64ce24
emyr2022 <emyr@students.amikom.ac.id>
2023-11-01 22:15:11 +0700

    first commit

--- dashboard/index.php
+++ dashboard/index.php
@@ -3,11 +3,13 @@ require_once '../layout/_top.php';
 require_once '../helper/connection.php';
 
 $mahasiswa = mysqli_query($connection, "SELECT COUNT(*) FROM mahasiswa");
+$rps = mysqli_query($connection, "SELECT COUNT(*) FROM pertemuan");
 $dosen = mysqli_query($connection, "SELECT COUNT(*) FROM dosen");
 $matakuliah = mysqli_query($connection, "SELECT COUNT(*) FROM matakuliah");
 $nilai = mysqli_query($connection, "SELECT COUNT(*) FROM nilai");
 
 $total_mahasiswa = mysqli_fetch_array($mahasiswa)[0];
+$total_rps = mysqli_fetch_array($rps)[0];
 $total_dosen = mysqli_fetch_array($dosen)[0];
 $total_matakuliah = mysqli_fetch_array($matakuliah)[0];
 $total_nilai = mysqli_fetch_array($nilai)[0];
@@ -49,6 +51,21 @@ $total_nilai = mysqli_fetch_array($nilai)[0];
           </div>
         </div>
       </div>
+      <div class="col-lg-3 col-md-6 col-sm-6 col-12">
+        <div class="card card-statistic-1">
+          <div class="card-icon bg-dark">
+            <i class="fas fa-book"></i>
+          </div>
+          <div class="card-wrap">
+            <div class="card-header">
+              <h4>RPS</h4>
+            </div>
+            <div class="card-body">
+              <?= $total_rps ?>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
     <div class="row">
 
@@ -82,6 +99,7 @@ $total_nilai = mysqli_fetch_array($nilai)[0];
           </div>
         </div>
       </div>
+      
     </div>
   </div>
 </section>
